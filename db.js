e0422ab771bad51a05fe32d87ea2a86bd17f2537
Vinicius Santos <vinicbs@gmail.com>
2019-06-12 02:14:58 -0300

    MINOR: database name to env var for heroku deploy

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const knex = require('knex')({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
-        database: 'bob',
+        database: process.env.DB_NAME,
         charset: 'utf8',
     }
 });
